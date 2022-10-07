9b20bbf535b2f2db787c319e1c0d6ae5139a30c6
Kogop <nesterovvlad100@gmail.com>
2022-10-07 16:41:13 +0300

    now looks better, no warnings

--- main.py
+++ main.py
@@ -1,56 +1,55 @@
 # This is a sample Python script.
-import random
+# import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-l = [0, 0, 0, 0, 0, 0, 0, 0]
+
+matrix = [0, 0, 0, 0, 0, 0, 0, 0]
 winning_strategy = [0, 4, 2, 6, 1, 5, 7, 3]
 
 
-def startORend(a):
-    if (l[7] != 1):
-        l[7] = 1
+def start_or_end(a):
+    if matrix[7] != 1:
+        matrix[7] = 1
     else:
-        l[7] = 0
+        matrix[7] = 0
 
-    if (l[0] != 1):
-        l[0] = 1
+    if matrix[0] != 1:
+        matrix[0] = 1
     else:
-        l[0] = 0
+        matrix[0] = 0
 
     if a == 0:
-        if (l[1] != 1):
-            l[1] = 1
+        if matrix[1] != 1:
+            matrix[1] = 1
         else:
-            l[1] = 0
+            matrix[1] = 0
 
     if a == 7:
-        if (l[6] != 1):
-            l[6] = 1
+        if matrix[6] != 1:
+            matrix[6] = 1
         else:
-            l[6] = 0
+            matrix[6] = 0
 
 
 def change_dot(a):
-    # while (l.count(1) != 8 or l.count(0) != 8):
+    # while (matrix.count(1) != 8 or matrix.count(0) != 8):
     for u in winning_strategy:
         # print(a)
         k = u + a
         while k >= 8:
             k = k - 8
         if k == 0 or k == 7:
-            startORend(k)
+            start_or_end(k)
         else:
             for o in range(0, 3):
-                if l[k - 1 + o] != 1:
-                    l[k - 1 + o] = 1
+                if matrix[k - 1 + o] != 1:
+                    matrix[k - 1 + o] = 1
                 else:
-                    l[k - 1 + o] = 0
+                    matrix[k - 1 + o] = 0
         # a = random.randint(0, 7)
 
-        print(l[0], " ",  l[1], ' ', l[2], '\n')
-        print(l[7], " ", " ", ' ', l[3], '\n')
-        print(l[6], " ", l[5], ' ', l[4], '\n')
+        print(matrix[0], " ", matrix[1], ' ', matrix[2], '\n')
+        print(matrix[7], " ", " ", ' ', matrix[3], '\n')
+        print(matrix[6], " ", matrix[5], ' ', matrix[4], '\n')
 
 
 # Press the green button in the gutter to run the script.
